f2bcf1b8268cf6d46db46951e08a977a77b3e83e
Ihor Prolazov <igorprolazov@gmail.com>
2024-01-28 19:10:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let userName = prompt(`What is your name?`, `Name`);
+let userSurname = prompt(`What is your surname?`, `Surname`);
+let userMiddleName = prompt(`What is your middle name?`, `Middle name`);
+alert(
+  `Hello ${userSurname} ${userName} ${userMiddleName}. I'm glad to meet you!`
+);
